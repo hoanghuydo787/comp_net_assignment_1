b5fe25b41fc9cead7f08725fa45b0c35d69aaf54
voquyen0502 <54432044+voquyen0502@users.noreply.github.com>
2022-11-28 09:08:56 +0700

    Update center-server.py

--- center-server.py
+++ center-server.py
@@ -1,3 +1,4 @@
+# sample code
 from socket import *
 import threading
 import pickle
@@ -13,9 +14,9 @@ class Server:
     LOGIN = 'LOGIN'
     LOGOUT = 'LOGOUT'
     SIGNUP = 'SIGNUP'
-    RETRIEVE_LIST_FRIEND =  'RETRIEVE LIST FRIEND'
+    FRIENDS_LIST = 'FRIENDS_LIST'
 
-    MAX_NUM_CLIENTS = 3
+    MAX_NUM_CLIENTS = 10
 
     """{<username>: {'password' :<password>,
                    'list_friends': [<username_friend1>,<username_friend2>,...]},
@@ -41,7 +42,8 @@ class Server:
 
     def create_listening_server(self):
         self.server_socket = socket(AF_INET, SOCK_STREAM) #create a socket using TCP port and ipv4
-        server_host = gethostbyname(gethostname())
+        # server_host = gethostbyname(gethostname())
+        server_host = "127.0.0.1"
         server_port = 12000
         # this will allow you to immediately restart a TCP server
         self.server_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
@@ -50,12 +52,6 @@ class Server:
         print("the server is ready to receive..")
         self.server_socket.listen(self.MAX_NUM_CLIENTS) #listen for incomming connections / max 5 clients
         self.receive_messages_in_a_new_thread()
-
-    def isOnline(self,username):
-        try:
-            return self.user_status[username]['status'] == 'online'
-        except:
-            raise f"{username} chưa được lưu trữ"
 
     def processRequest(self,request):
         token = request.split('|')
@@ -67,80 +63,89 @@ class Server:
                 return name
         return None
 
-    def lookup(self,ip,port):
-        for client in self.clients_list:
-            if client[1] == (ip,int(port)):
-                return client
+    def lookup(self, ip, port):
+        for name in self.user_logins:
+            if self.user_logins[name][1] == (ip, int(port)):
+                return name
         return None
 
-    def requestConnection(self,client,dest_ip,dest_port):
-        so,_ = self.lookup(dest_ip,dest_port)
-        msg = self.REQUEST_CONNECTION + f'|{client[1][0]},{client[1][1]}'
-        self.sendMessage(so,msg)
+    def requestConnection(self, src, des):
+        conn = self.user_logins[des][0]
+        msg = (self.REQUEST_CONNECTION, (src,))
+        self.sendMessage(conn, msg)
 
-    def rejectConnection(self,client,dest_ip,dest_port):
-        so,_ = self.lookup(dest_ip, dest_port)
-        msg = self.REJECT_CONNECTION + f'|{client[1][0]},{client[1][1]}'
-        self.sendMessage(so,msg)
+    def rejectConnection(self, src, des):
+        conn = self.user_logins[des][0]
+        msg = (self.REJECT_CONNECTION, (src,))
+        self.sendMessage(conn, msg)
 
-    def acceptConnection(self,client,dest_ip,dest_port):
-        so,_ = self.lookup(dest_ip, dest_port)
-        msg = self.ACCEPT_CONNECTION + f'|{client[1][0]},{client[1][1]}'
-        self.sendMessage(so,msg)
+    def acceptConnection(self, src, des):
+        conn = self.user_logins[des][0]
+        msg = (self.ACCEPT_CONNECTION, (src,))
+        self.sendMessage(conn, msg)
+
+    def updateStatus(self,username):
+        friends_list = self.database[username]['list_friends']
+        for friend in friends_list:
+            if self.isOnline(friend):
+                self.sendListFriend(friend)
+
+    def isOnline(self,username):
+        return username in self.user_logins
 
     def sendListFriend(self,username):
-        list_friends = []
+        list_friends = {}
         for name in self.database[username]['list_friends']:
             ip = port = None
-            status = 'offline'
-            if name in self.user_logins:
+            status = 'OFFLINE'
+            if self.isOnline(name):
                 ip,port = self.user_logins[name][1]
-                status = 'online'
-            list_friends.append([name,ip,port,status])
-        so = self.user_logins[username][0]
-        frlist = pickle.dumps(list_friends)
-        so.sendall(frlist)
+                status = 'ONLINE'
+            list_friends[name]=[ip,port,status]
+        conn = self.user_logins[username][0]
+        self.sendMessage(conn,(self.FRIENDS_LIST,(list_friends,)))
 
-    def signup(self,conn):
-        username = conn.recv(1024).decode()
-        password = conn.recv(1024).decode()
+    def signup(self,conn,username,password):
         if username not in self.database:
             self.database[username] = {'password':password,'list_friends':[]}
-            self.sendMessage(conn,'Success')
+            self.sendMessage(conn,'SUCCESS')
             with open("database.json", "w") as outfile:
                 json.dump(self.database, outfile)
             return True
         else:
-            self.sendMessage(conn,'Fail')
+            msg = (self.SIGNUP, ('FAIL',))
+            self.sendMessage(conn, msg)
             return False
 
 
-    def login(self, client):
+    def login(self, client, username,password):
         conn, _ = client
-        username = conn.recv(1024).decode()
-        password = conn.recv(1024).decode()
+        time.sleep(0.1)
+        msg = conn.recv(1024)
+        ip, port = pickle.loads(msg)
         if  self.authenticate(username,password):
-            self.user_logins[username] = client
-            self.sendMessage(conn,'Success')
+            self.user_logins[username] = (conn, (ip, int(port)))
+            msg = (self.LOGIN, ('SUCCESS',))
+            self.sendMessage(conn, msg)
             time.sleep(0.1)
             self.sendListFriend(username)
+            self.updateStatus(username)
             return True
         else:
-            self.sendMessage(conn,'Fail')
+            msg = (self.LOGIN, ('FAIL',))
+            self.sendMessage(conn, msg)
             return False
 
     def authenticate(self, username, password):
         return username in self.database and self.database[username]['password'] == password
 
     def sendMessage(self,conn,msg):
-        conn.sendall(msg.encode())
+        conn.sendall(pickle.dumps(msg))
 
-    def disconnectClient(self,client):
-        self.clients_list.remove(client)
-        ip, port = client[1]
-        username = self.username(ip,port)
+    def disconnectClient(self,username):
         if username:
             self.user_logins.pop(username)
+        self.updateStatus(username)
 
         print(f'Disconnect to ', ip, ':', str(port))
         client[0].close()
@@ -149,41 +154,35 @@ class Server:
         so, _ = client
         # login/signup
         while True:
-            """try:
-                rqst = so.recv(256) #initialize the buffer
-            except:
-                self.disconnectClient(client)
-                return"""
-            rqst = so.recv(256)
-            rqst = rqst.decode()
-            if  rqst == self.LOGIN:
-                if self.login(client): break
-            elif rqst == self.SIGNUP:
-                self.signup(so)
-
-        #username = self.username(*client[1])
-        """while True:
             try:
-                incoming_buffer = so.recv(256) #initialize the buffer
+                msg = so.recv(256) #initialize the buffer
             except:
                 self.disconnectClient(client)
                 return
-            request = incoming_buffer.decode('utf-8')
-            rqst, args = self.processRequest(request)
+            rqst, args = pickle.loads(msg)
+            if  rqst == self.LOGIN:
+                if self.login(client, *args):
+                    username = args[0]
+                    break
+            elif rqst == self.SIGNUP:
+                self.signup(so, *args)
+
+        while True:
+            try:
+                msg = so.recv(512) #initialize the buffer
+            except:
+                self.disconnectClient(client)
+                return
+            rqst, args = pickle.loads(msg)
             if rqst == self.REQUEST_CONNECTION:
-                dest_ip,des_port = args
-                self.requestConnection(client,dest_ip,des_port)
+                self.requestConnection(*args)
             elif rqst == self.REJECT_CONNECTION:
-                est_ip, des_port = args
-                self.rejectConnection(client,dest_ip,des_port)
+                self.rejectConnection(*args)
             elif rqst == self.ACCEPT_CONNECTION:
-                est_ip, des_port = args
-                self.acceptConnection(client,dest_ip,des_port)
-            elif rqst == self.RETRIEVE_LIST_FRIEND:
-                self.sendListFriend(username)
+                self.acceptConnection(*args)
             else:
-                pass
-            """
+                raise "request error"
+
 
     #broadcast the message to all clients
     def broadcast_to_all_clients(self, senders_socket):
